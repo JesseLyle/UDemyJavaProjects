c1e36225af385a7915b0111015d2942a098e61f1
Jesse <lylejesse142@gmail.com>
2021-04-28 17:44:27 -0500

    Moving Project Information Over. Nearing Beginning

---
+++ HangmanGame/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+/*
+ HangMan game
+ ask for name
+ Computer thinks of a word
+    Loop Begins
+Player Guesses letter
+Computer checks query to elements in word array
+    If Letter matches, Print out letter in element spots (Maybe use another array to bring in elements from existing word)
+    If Letter doesn't match, Print out next Hangman Part and Letter guessed so Player knows not to guess again
+    If letter was already used in guesses and was wrong, (Use another Array to store guessed and wrong letters) Ask for another input
+After 3 (Or however Many) Failed guesses, Player loses
+
+
+*/
+
+
+    public static void main(String[] args) {
+
+    }
+}
